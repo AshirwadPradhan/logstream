02b6654afba209a047e9169c889470be290af4ac
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2023-12-16 00:22:28 +0530

    broadcast to all clients

--- server.py
+++ server.py
@@ -3,16 +3,26 @@ import asyncio
 import datetime
 import random
 
-async def show_time(websocket: websockets.WebSocketServerProtocol):
+connections = set()
+
+async def register(websocket: websockets.WebSocketServerProtocol):
+    connections.add(websocket)
+    try:
+        await websocket.wait_closed()
+        print("Connection closed")
+    finally:
+        connections.remove(websocket)
+
+async def show_time():
     while True:
         message = datetime.datetime.utcnow().isoformat()
-        await websocket.send(message)
+        websockets.broadcast(connections, message)
         await asyncio.sleep(random.random()*2 + 1)
 
 
 async def main():
-    async with websockets.serve(show_time, "localhost", 8765):
-        await asyncio.Future()
+    async with websockets.serve(register, "localhost", 8765):
+        await show_time()
 
 if __name__ == "__main__":
     asyncio.run(main())
